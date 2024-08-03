345373186de13c77b8812d7cf9dc6dc84db95607
Mohamed Ali <mo7amedaliebaid@gmail.com>
2024-08-03 14:09:11 +0300

    test

--- test/news_change_notifier_test.dart
+++ test/news_change_notifier_test.dart
@@ -23,4 +23,9 @@ void main() {
     mockNewsService = MockNewsService();
     sut = NewsChangeNotifier(mockNewsService);
   });
+
+  test("Initial values are correct", () {
+    expect(sut.articles, []);
+    expect(sut.isLoading, false);
+  });
 }
