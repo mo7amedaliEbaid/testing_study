0ff6c00318cdd49ddce914a85f66bb831fb61cdb
Mohamed Ali <mo7amedaliebaid@gmail.com>
2024-08-03 15:18:13 +0300

    test

--- lib/news_change_notifier.dart
+++ lib/news_change_notifier.dart
@@ -3,20 +3,25 @@ import 'package:flutter/material.dart';
 import 'article.dart';
 import 'news_service.dart';
 
-
 class NewsChangeNotifier extends ChangeNotifier {
   final NewsService _newsService;
 
   NewsChangeNotifier(this._newsService);
 
-  final List<Article> _articles = [];
+  List<Article> _articles = [];
 
   List<Article> get articles => _articles;
 
-  final bool _isLoading = false;
+  bool _isLoading = false;
+
   bool get isLoading => _isLoading;
 
   Future<void> getArticles() async {
-    // TODO: Implement
+    _isLoading = true;
+    notifyListeners();
+    await _newsService.getArticles();
+    _articles = await _newsService.getArticles();
+    _isLoading = false;
+    notifyListeners();
   }
 }
--- test/news_change_notifier_test.dart
+++ test/news_change_notifier_test.dart
@@ -28,4 +28,32 @@ void main() {
     expect(sut.articles, []);
     expect(sut.isLoading, false);
   });
+
+  group("getArticles", () {
+    final articlesFromService = [
+      Article(title: "title1", content: "content1"),
+      Article(title: "title2", content: "content2"),
+      Article(title: "title3", content: "content3"),
+    ];
+    void arrangeNewsServiceReturnsArticles() {
+      when(() => mockNewsService.getArticles())
+          .thenAnswer((_) async => articlesFromService);
+    }
+
+    test("gets Articles Using NewsService", () async {
+      arrangeNewsServiceReturnsArticles();
+      await sut.getArticles();
+      verify(() => mockNewsService.getArticles()).called(2);
+    });
+    test("""indicates loading of data,
+     set articles to the ones from the service, 
+     indicates that data is not being loaded anymore""", () async {
+      arrangeNewsServiceReturnsArticles();
+      final future = sut.getArticles();
+      expect(sut.isLoading, true);
+      await future;
+      expect(sut.articles, articlesFromService);
+      expect(sut.isLoading, false);
+    });
+  });
 }
